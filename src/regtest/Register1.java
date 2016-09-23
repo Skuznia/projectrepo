91c06027435cafeadc1a7b780d3d52198bb6c808
Samuel Kuznia <skuznia@sesame-mbp.cbeagan.org>
2016-09-23 16:21:32 -0500

    First commit

--- src/regtest/Register1.java
+++ src/regtest/Register1.java
@@ -9,24 +9,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 public class Register1 {
 	private static final Logger log4j = LogManager.getLogger(Register1.class.getName());
 	
 	public static void main(String[] args){
-		
 		log4j.trace("Initializing Program."); 
-		log4j.debug("Debug message."); 
-		
+		//log4j.debug("Debug message test."); 
+		String firstName = "John";
+		String lastName = "Doe";
 		Register1 test = new Register1();
-		test.runReg("John","Doe");
-		
-		log4j.info("Info message."); 
-		log4j.error("Error message");
+		log4j.trace("Running runReg(" + firstName + "," + lastName + ")");
+		test.runReg(firstName,lastName);
+		//log4j.info("Info message test."); 
+		//log4j.error("Error message test.");
 		
 	}
 	public void runReg(String first, String last){
-		
 		String firstName = first;
 		String lastName = last;
 		WebDriver driver = new ChromeDriver();
@@ -37,43 +35,68 @@ public class Register1 {
 	
 	public void regSurfing(WebDriver d){
 		WebDriverWait wait = new WebDriverWait(d, 10);
-		
+		log4j.trace("Directing to qa5.caringbridge.org");
 		//direct to qa5
 		d.get("https://qa5.caringbridge.org/");
-				
-		WebElement element = d.findElement(By.xpath("//a[@data-qa-id='global-nav-login']"));
-		//wait until we can find the element
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@data-qa-id='global-nav-login']")));
-		
-		//click redirects us to next register page
-		element.click();
-				
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@data-qa-id='profile-signin-signup']")));
-		//click redirects us to register page
-		d.findElement(By.xpath("//a[@data-qa-id='profile-signin-signup']")).click();
+		try{
+			WebElement element = d.findElement(By.xpath("//a[@data-qa-id='global-nav-login']"));
+			//wait until we can find the element
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@data-qa-id='global-nav-login']")));
+			log4j.trace("Directing to signin/register page.");
+			//click redirects us to next register page
+			element.click();
+			
+			log4j.trace("Directing to register page.");
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@data-qa-id='profile-signin-signup']")));
+			//click redirects us to register page
+			d.findElement(By.xpath("//a[@data-qa-id='profile-signin-signup']")).click();
+		}catch(Exception e){
+			log4j.error("Exception in refSurfing: NoSuchElementException");
+		}
 	}
 	
-	public String inputInfo(WebDriver d,String first, String last){
+	public String inputInfo(WebDriver d, String first, String last){
 		WebDriverWait wait = new WebDriverWait(d, 10);
+		log4j.trace("Initializing Emailgeneratorv2 class.");
 		Emailgeneratorv2 gen = new Emailgeneratorv2();
-		
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@data-qa-id='profile-signup-first-name']")));
-		//input first name
-		d.findElement(By.xpath("//input[@data-qa-id='profile-signup-first-name']")).sendKeys(first);
-		//input last name
-		d.findElement(By.xpath("//input[@data-qa-id='profile-signup-last-name']")).sendKeys(last);		
+		log4j.trace("Calling emailgen() from Emailgeneratorv2 object.");
 		//generate an email string
 		String email = gen.emailgen();
-		//print out the email string
-		System.out.println(email);
-		//register with email
-		d.findElement(By.xpath("//input[@data-qa-id='profile-signup-email']")).sendKeys(email);
-		//input password
-		d.findElement(By.xpath("//input[@data-qa-id='profile-signup-password']")).sendKeys("abc123");
-		//click on terms of agreement
-		d.findElement(By.xpath("//input[@data-qa-id='profile-signup-terms']")).click();
-		//click on register
-		d.findElement(By.xpath("//input[@data-qa-id='profile-signup-submit']")).click();
+		try{
+			log4j.trace("Inputting info.");
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@data-qa-id='profile-signup-first-name']")));
+			//input first name
+			d.findElement(By.xpath("//input[@data-qa-id='profile-signup-first-name']")).sendKeys(first);
+			//input last name
+			d.findElement(By.xpath("//input[@data-qa-id='profile-signup-last-name']")).sendKeys(last);		
+			
+			//register with email
+			d.findElement(By.xpath("//input[@data-qa-id='profile-signup-email']")).sendKeys(email);
+			//input password
+			d.findElement(By.xpath("//input[@data-qa-id='profile-signup-password']")).sendKeys("abc123");
+			//click on terms of agreement
+			d.findElement(By.xpath("//input[@data-qa-id='profile-signup-terms']")).click();
+			//click on register
+			d.findElement(By.xpath("//input[@data-qa-id='profile-signup-submit']")).click();
+			log4j.trace("Submitting account registration.");
+		}catch(Exception e){
+			log4j.error("Exception in inputInfo: " + e);
+		}
 		return email;
+		
 	}
+	public boolean verifyLogin(WebDriver d){
+		WebDriverWait wait = new WebDriverWait(d,10);
+		try{
+			//check if the sign in (aka not logged in) button is there.
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@data-qa-id='global-nav-home']")));
+			if(d.findElements(By.xpath("//a[@href='https://qa5.caringbridge.org/signin?returl=%2F']")).size() != 0){
+				return false;
+			}
+		}catch(Exception e){
+			log4j.error("Exception in verifyLogin: " + e);
+		}
+		return true;
+	}
+	
 }
--- src/regtest/Register1Test.java
+++ src/regtest/Register1Test.java
@@ -8,43 +8,52 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Register1Test {
+	private static final Logger log4j = LogManager.getLogger(Register1.class.getName());
 
 	@Test
 	public void test() {
+		log4j.trace("Initializing Program.");
 		String firstName = "John";
 		String lastName = "Doe";
 		Register1 test = new Register1();
 		WebDriver driver = new ChromeDriver();
 		WebDriverWait wait = new WebDriverWait(driver, 10);
+		log4j.trace("Running regSurfing.");
 		test.regSurfing(driver);
+		log4j.trace("Running inputInfo.");
 		String email = test.inputInfo(driver, firstName, lastName);
-		
-		//check if the sign in (aka not logged in) button is there.
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@data-qa-id='global-nav-home']")));
-		if(driver.findElements(By.xpath("//a[@href='https://qa5.caringbridge.org/signin?returl=%2F']")).size() != 0){
+		log4j.trace("Verifying Login Successful, initializing verifyLogin().");
+		if(test.verifyLogin(driver) == false){
 			fail("Failed to successfully sign in.");
+			driver.quit();
 		}
-		
-		//heading to profile
-		driver.findElement(By.xpath("//a[@data-qa-id='global-nav-home']")).click();
-		//verifying that the registration process completed successful
-		driver.get("https://qa5.caringbridge.org/settings");
-				
-		//checking firstName
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='firstName']")));
-		WebElement element = driver.findElement(By.xpath("//input[@id='firstName']"));
-		
-		assertEquals("First Name differnet from the one entered.",firstName,element.getAttribute("value"));
-		//checking last name
-		element = driver.findElement(By.xpath("//input[@id='lastName']"));
-		assertEquals("Last Name differnet from the one entered.",lastName,element.getAttribute("value"));
-		
-		//checking email
-		element = driver.findElement(By.xpath("//input[@id='email']"));
-		assertEquals("Email differs from the one entered.",email,element.getAttribute("value"));
-		
+		try{
+			//heading to profile
+			driver.findElement(By.xpath("//a[@data-qa-id='global-nav-home']")).click();
+			//verifying that the registration process completed successful
+			log4j.trace("Directing to qa5.caringbridge.org/settings");
+			driver.get("https://qa5.caringbridge.org/settings");	
+			//checking firstName
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='firstName']")));
+			log4j.trace("Verifying all the expected information matches the real information.");
+			WebElement element = driver.findElement(By.xpath("//input[@id='firstName']"));
+			assertEquals("First Name differnet from the one entered.",firstName,element.getAttribute("value"));
+			//checking last name
+			element = driver.findElement(By.xpath("//input[@id='lastName']"));
+			assertEquals("Last Name differnet from the one entered.",lastName,element.getAttribute("value"));
+			
+			//checking email
+			element = driver.findElement(By.xpath("//input[@id='email']"));
+			assertEquals("Email differs from the one entered.",email,element.getAttribute("value"));
+		}catch(Exception e){
+			log4j.error("Exception found in test(): " + e);
+			driver.quit();
+			fail("Something went wrong..");
+		}
 		driver.quit();
 		
 		//fail("Not yet implemented");
